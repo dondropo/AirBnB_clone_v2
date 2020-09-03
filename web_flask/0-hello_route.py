62005e7ead87ee9a3b9aaec2877fd1d8df3bf7e6
Alejandro Rusca Moreno <alejandroruscamoreno@gmail.com>
2020-09-02 21:06:13 -0500

    [Fix]

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
+    """Returns 'Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
